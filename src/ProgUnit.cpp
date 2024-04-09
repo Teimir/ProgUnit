fe427a50c3a5ace24e3a33b151ccb25f44638d0d
Павел <pablo.sokolov@mail.ru>
2024-04-10 00:48:59 +0300

    test unification

--- src/ProgUnit.cpp
+++ src/ProgUnit.cpp
@@ -60,7 +60,40 @@ int mass_test_sync(ComIface& c) {
     }
     return failures;
 }
-
+/*
+* tests port RX-TX connection, N is array length, it_num is number of test
+*/
+template<typename T>
+bool test(ComIface& c_rx, ComIface& c_tx, T* transmit, int N, int it_num = 0) {
+    //return true if failed
+    byte* recieve = new byte[sizeof(T) * N]{0};
+    bool failed = true;
+    DWORD state = c_tx.write(transmit, sizeof(T) * N);
+    if (state == sizeof(T) * N) {
+        state = c_rx.read(recieve, 1);
+        if (state == sizeof(T) * N) {
+            std::reverse(recieve, recieve + sizeof(T) * N);
+            failed = memcmp(transmit, recieve, sizeof(T) * N);
+        }
+        else {
+            printf("Read failed (%d/%zd)!\t", state, sizeof(T) * N);
+        }
+    }
+    else {
+        printf("Write failed (%d/%zd)!\t", state, sizeof(T) * N);
+    }
+    printf("%d\tTranslated - ", it_num);
+    for (int i = 0; i < sizeof(T) * N; ++i) {
+        printf("%02hhx", ((byte*)transmit)[i]);
+    }
+    printf(",\trecieved - ");
+    for (int i = 0; i < sizeof(T) * N; ++i) {
+        printf("%02hhx", recieve[i]);
+    }
+    printf("\t%s", failed ? "NO\n" : "YES\n");
+    delete[] recieve;
+    return failed;
+}
 
 
 
@@ -71,13 +104,13 @@ int _tmain(int argc, TCHAR* argv[]) {
     byte data[] = { 0x1b, 0x1c, 0x12, 0x25 };
     byte edata[8][8] = {
         {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07},
-        {0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
-        {0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
-        {0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
-        {0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
-        {0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
-        {0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
-        {0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15}
+        {0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f},
+        {0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17},
+        {0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f},
+        {0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27},
+        {0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f},
+        {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37},
+        {0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f}
     }; //to do - fill
     byte sw_ch[] = { 0xff, 0xf2 };
     byte rw_ch[] = { 0xff, 0xf1 };
@@ -105,24 +138,9 @@ int _tmain(int argc, TCHAR* argv[]) {
     comiface.log_state();
     int c = 0;
     for (int i = 0; i < numOfTests; ++i) {
-        if (!comiface.write(&data[i % 4], 1)) {
-            printf("Write failed!\t");
-        };
-        if (!comiface.read(&buffer, 1)) {
-            printf("Read failed!\t");
-        };
-        printf(
-            "%d\tTranslated - %x,\trecieved - %x\t%s",
-            i,
-            data[i % 4],
-            buffer,
-            data[i % 4] == buffer ? "YES\n" : "NO\n"
-        );
-        c = data[i % 4] != buffer ? c + 1 : c;
+        c = test(comiface, comiface, &data[i % 4], 1, i) ? c + 1 : c;
     }
     printf("Errors - %d / Tests - %d\n", c, numOfTests);
-
-
     //change rate
     comiface.write(&sw_ch[0], 1);
     comiface.read(&buffer, 1);
@@ -140,20 +158,7 @@ int _tmain(int argc, TCHAR* argv[]) {
     c = 0;
     //_tprintf (TEXT("Serial port %s successfully reconfigured.\n"), pcCommPort);
     for (int i = 0; i < numOfTests; i++) {
-        if (!comiface.write(&data[i % 4], 1)) {
-            printf("Write failed!\t");
-        };
-        if (!comiface.read(&buffer, 1)) {
-            printf("Read failed!\t");
-        };
-        printf(
-            "%d\tTranslated - %x,\trecieved - %x\t%s",
-            i,
-            data[i % 4],
-            buffer,
-            data[i % 4] == buffer ? "YES\n" : "NO\n"
-        );
-        c = data[i % 4] != buffer ? c + 1 : c;
+        c = test(comiface, comiface, &data[i % 4], 1, i) ? c + 1 : c;
     }
     printf("Errors - %d / Tests - %d\n", c, numOfTests);
 
@@ -166,23 +171,7 @@ int _tmain(int argc, TCHAR* argv[]) {
     c = 0;
     //test with 8 byte write/read
     for (int i = 0; i < numOfTests; ++i) {
-        if (ARRAYSIZE(edata[i % 8]) != comiface.write(edata[i % 8], ARRAYSIZE(edata[i % 8]))) {
-            printf("Write failed!\t");
-        }
-        if (ARRAYSIZE(ebuffer) == comiface.read(ebuffer, ARRAYSIZE(ebuffer))) {
-            std::reverse(ebuffer, ebuffer + ARRAYSIZE(ebuffer));
-        }
-        else {
-            printf("Read failed!\t");
-        }
-        printf(
-            "%d\tTranslated - %x,\trecieved - %x\t%s",
-            i,
-            edata[i % 8],
-            ebuffer,
-            !memcmp(edata[i % 8], ebuffer, ARRAYSIZE(ebuffer)) ? "YES\n" : "NO\n"
-        );
-        c = data[i % 4] != buffer ? c + 1 : c;
+        c = test(comiface, comiface, edata[i % 8], ARRAYSIZE(edata[i % 8]), i) ? c + 1 : c;
     }
     printf("Errors - %d / Tests - %d\n", c, numOfTests);
 
