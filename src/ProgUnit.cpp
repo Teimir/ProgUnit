3de5827b612faf35a2edce3f672bc9878cb9fb7a
Павел <pablo.sokolov@mail.ru>
2024-04-09 20:22:56 +0300

    add 8 byte write/read tests

--- src/ProgUnit.cpp
+++ src/ProgUnit.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <tchar.h>
 #include <stdio.h>
+#include <algorithm>
 //Локальные заголовки
 #include "ComIface.h"
 
@@ -68,9 +69,11 @@ int _tmain(int argc, TCHAR* argv[]) {
     int numOfTests = 0;
     ComIface comiface(CBR_9600);
     byte data[] = { 0x1b, 0x1c, 0x12, 0x25 };
+    byte edata[8][8]; //to do - fill
     byte sw_ch[] = { 0xff, 0xf2 };
     byte rw_ch[] = { 0xff, 0xf1 };
     byte buffer = 0;
+    byte ebuffer[8];
     for (int i = 0; i < 256; i++) {
         if (comiface.open(i, false)) {
             printf("COM %d id avalible! \n", i);
@@ -114,15 +117,15 @@ int _tmain(int argc, TCHAR* argv[]) {
     //change rate
     comiface.write(&sw_ch[0], 1);
     comiface.read(&buffer, 1);
-    printf("recieved - %x\t\n", buffer);
+    printf("recieved - %x\n", buffer);
     comiface.write(&sw_ch[1], 1);
     comiface.read(&buffer, 1);
-    printf("recieved - %x\t\n", buffer);
-    while (buffer == 0) {
-        comiface.write(&sw_ch[1], 1);
-        comiface.read(&buffer, 1);
-        printf("recieved - %x\t\n", buffer);
-    }
+    printf("recieved - %x\n", buffer);
+    //while (buffer == 0) {
+    //    comiface.write(&sw_ch[1], 1);
+    //    comiface.read(&buffer, 1);
+    //    printf("recieved - %x\t\n", buffer);
+    //}
     comiface.set_rate(CBR_115200);
     comiface.log_state();
     c = 0;
@@ -147,10 +150,34 @@ int _tmain(int argc, TCHAR* argv[]) {
 
     comiface.write(&rw_ch[0], 1);
     comiface.read(&buffer, 1);
-    printf("recieved - %x     ", buffer);
+    printf("recieved - %x\n", buffer);
     comiface.write(&rw_ch[1], 1);
     comiface.read(&buffer, 1);
-    printf("recieved - %x     ", buffer);
+    printf("recieved - %x\n", buffer);
+    c = 0;
+    //test with 8 byte write/read
+    for (int i = 0; i < numOfTests; ++i) {
+        if (ARRAYSIZE(edata[i % 8]) != comiface.write(edata[i % 8], ARRAYSIZE(edata[i % 8]))) {
+            printf("Write failed!\t");
+        }
+        if (ARRAYSIZE(ebuffer) == comiface.read(ebuffer, ARRAYSIZE(ebuffer))) {
+            std::reverse(ebuffer, ebuffer + ARRAYSIZE(ebuffer));
+        }
+        else {
+            printf("Read failed!\t");
+        }
+        printf(
+            "%d\tTranslated - %x,\trecieved - %x\t%s",
+            i,
+            edata[i % 8],
+            ebuffer,
+            memcmp(edata[i % 8], ebuffer, ARRAYSIZE(ebuffer)) ? "YES\n" : "NO\n"
+        );
+        c = data[i % 4] != buffer ? c + 1 : c;
+    }
+    printf("Errors - %d / Tests - %d\n", c, numOfTests);
+
+
     //mass_test_sync(comiface);
     comiface.close();
     getchar();
